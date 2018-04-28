f1737ffdb809fb4da3707afdb5474d98e892384d
cui <1914579410@qq.com>
2018-04-28 15:48:34 +0800

    添加axios拦截器

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://127.0.0.1:8001',
+        target: 'http://127.0.0.1:8005',
         changeOrigin: true,
       }
     },
---
+++ src/http/axios.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import axios from 'axios'
+// import qs from 'qs'
+// import { currencyType } from '../utils/global-filters';
+
+export function createHttp() {
+  Vue.prototype.$http = axios
+  initInterceptersRequest()
+  initInterceptersResponse()
+}
+var configArr = [];
+const initInterceptersRequest = () => {
+  axios.interceptors.request.use(function (config) {
+    // console.log(config, '请求')
+    return config
+  })
+}
+
+const initInterceptersResponse = () => {
+  axios.interceptors.response.use(function (response) {
+    if (parseInt(response.data.code) === 9000 || parseInt(response.data.respCode) === 9000) {
+      return response
+    } else {
+      var msg = response.data.respDesc ? response.data.respDesc : response.data.message
+      // vue.$message({ message: msg, type: 'warning' });
+      return Promise.reject({})
+    }
+  }, function (error) {
+    // Do something with response error
+    // if (error.response.status === 400) {
+    //   // alert('参数错误')
+    // } else if (error.response.status === 401) {
+    //   if (!window.location.href.match('/login')) {
+    //     //router.push('/login')
+    //   }
+    // } else if (error.response.status === 403
+    //   || error.response.status === 404
+    //   || error.response.status === 500) {
+    //   router.push(`/status/${error.response.status}`)
+    // }
+    // return Promise.reject(error)
+  })
+}
